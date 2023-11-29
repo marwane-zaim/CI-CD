9057db45020319b986770e6e74e638fb64556ac3
ZAIM-SASSI Marwane <ptmmzi1@MAFELEC-TEAM.lan>
2023-11-29 16:17:08 +0100

    Question Renommage

--- run.py
+++ run.py
@@ -7,7 +7,32 @@ def main():
     """
     Main
     """
-    pass
+    data = import_data()
+    renommage_colonnes(data)
+
+    
+def import_data():
+    """
+    Import csv file as a dataframe
+    Output: data [pd.DataFrame]
+    """
+    data = pd.read_csv("data/iris.csv")
+    print(data.shape)
+    return data
+
+def renommage_colonnes(data:pd.DataFrame)-> pd.DataFrame:
+    """
+    Rennomage des colonnes
+    Import:csv file as a dataframe
+    Output: data [pd.DataFrame]
+    """
+    data_renomme = data.rename({"sepal.length":"sepal_length",
+                                "sepal.width":"sepal_width",	
+                                "petal.length":"petal_length", 
+                                "petal.width":"petal_width",
+                                "variety":"variety"
+                                })
+    return data_renomme
 
 
 if __name__ == '__main__':
--- test_run.py
+++ test_run.py
@@ -1,3 +1,21 @@
 """
 Tests
 """
+from run import import_data, renommage_colonnes
+
+def test_import_data():
+    """
+    Test
+    """
+    data = import_data()
+    assert data.shape[0] > 0
+
+
+def test_renommage_colonnes():
+    """
+    Test
+    """
+    data = import_data()
+    data_renomme = renommage_colonnes(data)
+
+    assert "sepal_length" in data_renomme.columns
