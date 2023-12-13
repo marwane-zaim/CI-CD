1caba760bff36a47491a674ade72520b5bd2a4a1
ZAIM-SASSI Marwane <ptmmzi1@MAFELEC-TEAM.lan>
2023-12-13 09:42:34 +0100

    Corriger les erreurs Linting 2.0

--- run.py
+++ run.py
@@ -10,13 +10,11 @@ def main():
     """
     data = import_data()
     rename_columns(data)
-
     extrait_du_dataSet(data)
-
     multiplier_dataset(data)
 
-    
-def import_data() -> pd.DataFrame :
+
+def import_data() -> pd.DataFrame:
     """
     Import csv file as a dataframe
     Output: data [pd.DataFrame]
@@ -26,14 +24,14 @@ def import_data() -> pd.DataFrame :
     return data
 
 
-def rename_columns(data : pd.DataFrame) -> pd.DataFrame :
+def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
     """
     Input
         - data (pd.DataFrame): Le DataFrame d'entrée avec des noms de colonnes éventuellement non standard.
- 
+
     Output
         - pd.DataFrame: Un nouveau DataFrame avec des colonnes renommées selon des noms standard.
- 
+
     Description:
         Cette fonction prend en input le dataFrame et renomme ses colonnes avec des noms standard
         Les noms standard des colonnes sont :
@@ -41,26 +39,25 @@ def rename_columns(data : pd.DataFrame) -> pd.DataFrame :
         - 'sepal_width' pour la colonne 'sepal.width'
         - 'petal_length' pour la colonne 'petal.length'
         - 'petal_width' pour la colonne 'petal.width'
- 
+
         La fonction utilise la méthode `rename` de pandas pour effectuer le renommage des colonnes.
     """
     data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
-                                "sepal.width": 'sepal_width',
-                                "petal.length": 'petal_length',
-                                "petal.width": 'petal_width'})
-    
+                                        "sepal.width": 'sepal_width',
+                                        "petal.length": 'petal_length',
+                                        "petal.width": 'petal_width'})
+
     return data_renamed
 
 
-def extrait_du_dataSet(data : pd.DataFrame)-> pd.DataFrame :
-
+def extrait_du_dataSet(data: pd.DataFrame) -> pd.DataFrame:
     """
     Cette fonction prend en entrée un DataFrame 'data' et renvoie un nouvel échantillon
     de 50 lignes sélectionnées de manière aléatoire à partir du DataFrame d'origine.
- 
+
     Input:
         data (pd.DataFrame): Le DataFrame de données à partir duquel extraire l'échantillon.
- 
+
     Output:
         pd.DataFrame: Un nouveau DataFrame contenant un échantillon de 50 lignes du DataFrame d'origine.
 
@@ -70,23 +67,21 @@ def extrait_du_dataSet(data : pd.DataFrame)-> pd.DataFrame :
     return data_sample
 
 
-def multiplier_dataset(data : pd.DataFrame)-> pd.DataFrame :
-
+def multiplier_dataset(data: pd.DataFrame) -> pd.DataFrame:
     """
     une fonction qui multiplie le nombre d'échantillons dans un DataFrame.
- 
-   INPUT
+
+    INPUT
     - data (pd.DataFrame): Le DataFrame d'entrée contenant les données à multiplier.
- 
+
     Output
     - pd.DataFrame: Un nouveau DataFrame avec le contenu répété trois fois.
- 
+
     Description:
     Cette fonction prend un DataFrame pandas en entrée, extrait un échantillon à l'aide de la fonction `extrait_du_dataSet`,
     puis réplique cet échantillon trois fois à l'aide de la fonction `pd.concat` avec l'option `ignore_index=True`.
 
     """
-
     data_sample = extrait_du_dataSet(data)
 
     data_triple = pd.concat([data_sample] * 3, ignore_index=True)
--- test_run.py
+++ test_run.py
@@ -3,6 +3,7 @@ Tests
 """
 from run import import_data, rename_columns, extrait_du_dataSet, multiplier_dataset
 
+
 def test_import_data():
     """
     Test
@@ -24,16 +25,15 @@ def test_extrait_du_dataSet():
     """
     Vérifier si les lignes prises égales à 50 lignes
     """
-    data =  import_data()
-    data_extrait =  extrait_du_dataSet(data)
+    data = import_data()
+    data_extrait = extrait_du_dataSet(data)
     assert len(data_extrait) == 50
 
 
-def test_multiplier_dataset():                                                          
+def test_multiplier_dataset():
     """
-    Verifier si les lignes sont multipliés est égal à 150 lignes 
+    Verifier si les lignes sont multipliés est égal à 150 lignes
     """
-    data =  import_data()
+    data = import_data()
     data_triple = multiplier_dataset(data)
     assert len(data_triple) == 150
-
